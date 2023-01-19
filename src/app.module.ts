8c3e49467e4dc2adaad11822c9766416adf8be52
svyat174 <svyatgluhov@gmail.com>
2023-01-19 22:04:41 +0300

    add imports and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeeModule } from './coffee/coffee.module';
 
 @Module({
-  imports: [CoffeeModule],
+  imports: [
+    CoffeeModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Dfcz2019',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/coffee/coffee.module.ts
+++ src/coffee/coffee.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoffeeService } from './coffee.service';
 import { CoffeeController } from './coffee.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
   providers: [CoffeeService],
   controllers: [CoffeeController]
 })
